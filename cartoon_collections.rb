e5dce11ce5c4b1a6362f0647235f5b4b285c4786
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-27 19:11:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,8 @@ end
 def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  first_cheese = nil
+  ingredients
   ingredients.include? 
   
 end
