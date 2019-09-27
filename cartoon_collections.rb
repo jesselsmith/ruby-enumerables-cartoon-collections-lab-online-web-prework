98491a88d6c6fc2f3814627c4dd490e0cfc224bf
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-27 19:09:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,10 @@ def long_planeteer_calls(words)
   }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  ingredients..include
+  
 end
